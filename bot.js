6d7a0888c553c29bd4597fac2345a3c160deffd2
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-19 05:39:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -831,62 +831,13 @@ var prefix = "$";
 
 
 
-hero.on("message",async message => {
-  var one;
-  var two;
-  var three;
-  var aa;
-  var prefix = "$"
-  if(message.content.startsWith(prefix + "slots")) {
-      if(message.content.split(' ')[0] !== `${prefix}slots`) return;
-    var first = ["🍊", "🍇", "🍒", "🍎", "🍋"];
-    var second = ["🍊", "🍇", "🍒", "🍎", "🍋"];
-    var third = ["🍊", "🍇", "🍒", "🍎", "🍋"];
-
-    one = first[Math.floor(Math.random () * first.length) + 0];
-    two = second[Math.floor(Math.random() * second.length) + 0];
-    three = third[Math.floor(Math.random() * third.length) + 0];
-    if(one === two && two === three) {
-      aa = "لقد فزت";
-    } else {
-      aa = "لقد خسرت";
-    }
-
-    message.channel.send(`**${one} | ${two} | ${three}**\n\n\`${aa}\`\n${message.author}`);
-  }
-});
 
 
 
 
 
 
-client.on('message',async message => {
-  if(message.author.bot || message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-var prefix = "$"
-  if(args[0] === `${prefix}bc`) {
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
-    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
-  
-    let msgCount = 0;
-    let errorCount = 0;
-    let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
-      message.guild.members.forEach(g => {
-        g.send(args.slice(1).join(' ')).then(() => {
-          successCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
-        }).catch(e => {
-          errorCount++;
-          msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
-        });
-      });
-    });
-  }
-});
+
 
 
 
