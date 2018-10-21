0522f70de1bf9d36b8fee7fd02de3f2e19937920
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-22 02:47:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -859,7 +859,7 @@ client.on('message', message => {
                                 .setColor("RANDOM")
                                 .setThumbnail(`${message.author.avatarURL}`)
                                 .addField('سبب التذكرة', args)
-                                .addField('اخرون', message.author)
+                                .addField('صاحب التكت', message.author)
                                 .addField('الروم', `<#${message.channel.id}>`);
 
                                 ticket.sendEmbed(embed);
@@ -919,6 +919,12 @@ var prefix = "$"
             message.channel.send(link);
         }
     });
+
+
+
+
+
+
 
 
 
