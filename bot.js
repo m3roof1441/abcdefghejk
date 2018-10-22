44df89813b9d97c3405a1f050a40b0d77ba3635f
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-22 23:02:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1038,29 +1038,25 @@ if(message.content.startsWith(prefix + 'ch')) {
 
 
 
-client.on('message',async Sal => { // By : M3roof
-    let embed = new Discord.RichEmbed()
-    if (Sal.content === "$id") {
-      let embed = new Discord.RichEmbed()
-     .setColor("RANDOM")
-     .setThumbnail(Sal.author.avatarURL)
-     .setImage(Sal.author.avatarURL)
-     .addField("اسمك:",`${Sal.author.username}`, true)
-     .addField('ايديك:',"" +  Sal.author.id, true)
-     .addField('تاقك', Sal.author.discriminator, true)
-     .addField('تم الانشاء في', Sal.author.createdAt, true)
-     Sal.channel.sendEmbed(embed);
-
-
-
-  
-
-
-
-
-  message.channel.sendEmbed(embed);
-    }
-});
+client.on('message', message => {
+    if (message.content === "$id") {
+    var year = message.createdAt.getFullYear()
+    var month = message.createdAt.getMonth()
+    var day = message.createdAt.getDate()
+         let embed = new Discord.RichEmbed()
+         .setAuthor(message.author.username, message.author.avatarURL)
+      .setThumbnail(message.author.avatarURL)
+        .addField("**اسمك:**",  '**[ ' + `${message.author.username}` + ' ]**')
+          .setThumbnail(message.author.avatarURL)
+                   .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+      .addField('الكود الخاص بك:', message.author.discriminator)
+      .addField("**عدد الايام منذ افتتاح حسابك:**", message.author.createdAt.getDate())
+        .addField("** تم افتتاح حسابك عام:**", message.createdAt.getFullYear())
+            .addField("** عدد الشهور منذ افتتاح حسابك:**", message.createdAt.getMonth())
+    
+      message.channel.send({embed});
+        }
+    });
 
 
 
