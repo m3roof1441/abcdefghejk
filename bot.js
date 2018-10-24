fee7ec231f9d62415d82a6ece83d2c4397bdc45c
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-24 04:50:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -381,7 +381,9 @@ client.on('message', message => {
 .addField('     ** → فتح الشات ←  ** ' ,' **  $op ** ')
 .addField('     ** → برودكاست ←  ** ' ,' **  $bc ** ')
 .addField('     ** → قيف اواي ←  ** ' ,' **  $giveaway ** ')
-
+      msg.channel.send({embed:embed});
+	    .setAuthor(message.author.username)
+  .addField(" تـم الارسال :white_check_mark:")
 
 
 
