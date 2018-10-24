f897df990dd12a39519d077b060478d4c035d778
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-25 01:13:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1170,15 +1170,15 @@ client.on('message', message => {
     let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
     let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
     reaction1.on("collect", r => {
-    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+    message.channel.send(`☑ | تم ... ارسال البرودكاست بنجاح ${message.guild.members.size} Members`).then(m => m.delete(5000));
     message.guild.members.forEach(m => {
     var bc = new
        Discord.RichEmbed()
        .setColor('RANDOM')
-       .setTitle('Broadcast')
-       .addField('Server', message.guild.name)
-       .addField('Sender', message.author.username)
-       .addField('Message', args)
+       .setTitle('برودكاست')
+       .addField('سيرفر', message.guild.name)
+       .addField('المرسل', message.author.username)
+       .addField('الرسالة', args)
        .setThumbnail(message.author.avatarURL)
        .setFooter(copy, client.user.avatarURL);
     m.send({ embed: bc })
@@ -1186,7 +1186,7 @@ client.on('message', message => {
     })
     })
     reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    message.channel.send(`**تم الغا البرودكاست.**`).then(m => m.delete(5000));
     msg.delete();
     })
     })
