c7e2630c7a8288a479cb4fad8bb017723518f678
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-29 06:43:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1261,10 +1261,7 @@ client.on("message", message => {
   
   });
 
-client.on('guildMemberAdd', member=> {
 
-    client.channels.get("501514924552617994").send(`***__ Welcome To Creative Fox Enjoy  ${member}___***`);
-    });
 
 
 client.login(process.env.BOT_TOKEN);
