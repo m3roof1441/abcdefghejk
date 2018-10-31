334883ade3680512a69c1453d7ddb74ed5a72bb1
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-31 04:18:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1296,20 +1296,6 @@ message.channel.send(embed);
     
 });
 
- client.on('message', message => {
-   if(message.content.startsWith("$invites")) {
-    message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-               let mmmmEmbed = new Discord.RichEmbed()
-                         .setAuthor(client.user.username)
-                         .setThumbnail(message.author.avatarURL)
- .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
-           .setFooter(`- Requested By: ${message.author.tag}`);
-           message.channel.send(mmmmEmbed)
-});
-  }
-});
+
 
 client.login(process.env.BOT_TOKEN);
