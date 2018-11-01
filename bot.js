d9186b0b10959352b357d30f130a502a2385c95e
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-11-01 08:33:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,35 @@ client.on('message' , message => {
 
 
 
+const discord_token = "NDY3NzgyNjQyNTQ5NjUzNTE0.DrqeJA.-zFXoVe7G8M3GhgpHb5Qww3kVp4";
+const prefix = "$"
+client.login(discord_token);
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '467782642549653514') return;
 
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
 
 
 
