b373fc8f44aea1e84196b4784eb564246b5e7636
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-11-01 04:18:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -976,7 +976,7 @@ client.on('message', message => {
 client.on("ready", () => {
   function lol() {
     client.guilds.get('501459185423220804').roles.find("name", "Rainbow").setColor("RANDOM");
-    client.guilds.get('501459185423220804').roles.find("name", "⦁✵- Fox_inviter").setColor("RANDOM");
+    client.guilds.get('501459185423220804').roles.find("name", "♛ Fox_inviter ♛").setColor("RANDOM");
   };
   setInterval(lol, 10000);
 });
